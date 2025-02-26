ddb9cd55aa93582fb8a42394f3538578dc78a975
codebender828 <codebender828@gmail.com>
2025-02-25 18:57:35 -0700

    feat: added contents for program treasury account

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,8 @@ export const SQUADS_V4_PROGRAM_ID = new PublicKey(
 
 export const DEFAULT_RPC_URL = "https://api.testnet.sonic.game";
 
+// This program treasury address is an account that is strictly necessary to declare when creating a new multisig address.
+// This account is statically initialized when the squads program is deployed. So do not change this.
 export const PROGRAM_TREASURY_ACCOUNT = new PublicKey(
   "4KhjyJEhy5ve55t9pdhTKmMTHZDvhzEp3GV2yazwgPzg"
 );
